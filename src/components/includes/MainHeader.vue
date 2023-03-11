5b0be813f13b6eb976d3dedcb62f4ca7c8df3602
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-03-11 10:27:02 -0500

    finish design header

--- src/components/includes/MainHeader.vue
+++ src/components/includes/MainHeader.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="header">
-        <h3>aqui va el header</h3>
+        <a href="#" class="header__logo">Mi catálogo</a>
+
+        <ul class="header__menu">
+            <li class="header__menu__item">
+                <a class="header__menu__item__link" href="">Productos</a>
+            </li>
+
+            <li class="header__menu__item">
+                <a class="header__menu__item__link" href="">Categorías</a>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -10,4 +20,59 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header {
+    background: $color-1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    @include desde ($medium) {
+        justify-content: space-between;
+    }
+
+    @include desde ($large) {
+        justify-content: space-between;
+        padding: 1em;
+    }
+
+    &__logo {
+        color: $color-2;
+        font-family: $ff-1;
+        font-size: 24px;
+        text-decoration: none;
+
+        @include desde ($medium) {
+            justify-content: space-between;
+            font-size: 40px;
+        }
+    }
+
+    &__menu {
+        display: none;
+
+        @include desde ($medium) {
+            display: flex;
+            list-style: none;
+            padding: 0px;
+            margin: 0px;
+
+            &__item {
+                margin: 15px;
+
+                &__link {
+                    display: block;
+                    padding: 5px 10px;
+                    text-decoration: none;
+                    color: $color-2;
+                    font-family: $ff-2;
+                    font-weight: bold;
+                    font-size: 18px;
+
+                }
+            }
+        }
+
+    }
+}
+</style>
